ad1446bdb7481179a51ca54c1dae9c10549a11aa
kumar.rishabh <kumar.rishabh@fyntune.com>
2026-08-21 12:48:04 +0530

    LeetCode2309 completed

--- src/LeetCode/LeetCode2309.java
+++ src/LeetCode/LeetCode2309.java
@@ -7,7 +7,7 @@ public class LeetCode2309 {
     static void main() {
 
         LeetCode2309 lc = new LeetCode2309();
-        System.out.println(lc.greatestLetter("lEeTcOdE"));
+        System.out.println(lc.greatestLetter("rgFGerFGHB"));
 
     }
 
@@ -16,21 +16,20 @@ public class LeetCode2309 {
         HashSet<Character> set = new HashSet<Character>();
         StringBuilder sb = new StringBuilder();
 
-        for(int i=0;i<s.length();i++)
-        {
-            set.add(s.charAt(i));
-        }
-
         for(int i=0; i<s.length();i++)
         {
             /*if(Character.isLowerCase(s.charAt(i)) &&  set.contains(Character.toUpperCase(s.charAt(i))))
             {
                 sb.append(s.charAt(i));
             }*/
-            if(Character.isUpperCase(s.charAt(i)) &&  set.contains(Character.toLowerCase(s.charAt(i)))){
+            if(Character.isUpperCase(s.charAt(i)) &&  set.contains(Character.toLowerCase(s.charAt(i))))
+            {
                 if(sb.isEmpty())
                 {
                     sb.append(s.charAt(i));
+                } else if (s.charAt(i) > sb.charAt(0))
+                {
+                    sb.setCharAt(0,s.charAt(i));
                 }
             }
         }
